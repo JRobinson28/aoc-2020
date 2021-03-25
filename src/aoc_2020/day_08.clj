5648a48cf90dee9b72bc04efe6b16f1cb2df69d8
JRobinson28 <joseph28robinson@gmail.com>
2021-03-25 16:45:51 +0000

    Day 8 finished

--- src/aoc_2020/day_08.clj
+++ src/aoc_2020/day_08.clj
@@ -4,13 +4,14 @@
 
 (def data (line-seq (io/reader (io/resource "day_08"))))
 
-(defn get-acc-1
+(defn get-acc
+  "Returns the final value of acc for a given instruction set."
   [instructions]
   (loop [acc 0
          i 0
          visited #{}]
-    (cond (contains? visited i) acc
-          (= (count instructions) i) acc
+    (cond (contains? visited i) (str acc)
+          (= (count instructions) i) (str "end " acc)
           :else (let [task (subs (nth instructions i) 0 3)
                       num (Integer/parseInt (subs (nth instructions i) 4))]
                   (case task
@@ -18,13 +19,27 @@
                     "jmp" (recur acc (+ i num) (conj visited i))
                     "nop" (recur acc (inc i) (conj visited i)))))))
 
-(defn swap-tasks
-  [line]
-  (let [task (subs line 0 3)
-        num (subs line 4)]
-    (if (= task "jmp")
-      (str "nop " num)
-      (str "jmp " num))))
+(defn get-variations
+  "Returns every possible permutation of swapping nop and jmp for a given instruction set."
+  [instructions]
+  (let [length   (count instructions)
+        swaps {"acc" "acc"
+               "nop" "jmp"
+               "jmp" "nop"}]
+    (loop [i 0
+           variations ()]
+      (cond (= i length)
+            (reverse variations)
+        :else (let [task (subs (nth instructions i) 0 3)
+                    num (Integer/parseInt (subs (nth instructions i) 4))]
+                (recur (inc i)
+                       (cons (assoc (vec instructions) i (str (swaps task) " " num)) variations)))))))
 
 ;; Part 1
-(get-acc data)
+(get-acc-1 data)
+
+;; Part 2
+(->> data
+     get-variations
+     (map get-acc)
+     (filter #(= (subs % 0 1) "e")))
